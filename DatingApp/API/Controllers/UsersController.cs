3299cbda67d46e67df55e935ea50b27e0ab35413
Victor Yampolsky <tsanhan@gmail.com>
2022-07-31 20:47:14 +0300

    16. Making our code Asynchronous

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,16 +19,16 @@ namespace API.Controllers
         }
 
         [HttpGet] // api/users
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-           var users = _context.Users.ToList();
+           var users = await _context.Users.ToListAsync();
            return users;
         }
 
         [HttpGet("{id}")] // api/users/1
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            var user = _context.Users.Find(id);
+            var user = await _context.Users.FindAsync(id);
             return user;
         }
     }
--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-creating our own controller:
-- lets see what we allready have in WeatherForecastController.cs => go to file.
-- lets create a controller => right click on Controllers folder => c# => Class.
-- create and go to UsersController.cs
+making our BE code async.
+go to UsersController.cs
 
-* test our api, go to https://localhost:5001/api/users
-* a better wat to test API is using postman
-* pay attention that the id is in camelCase, even though its TitleCate in our Entity.
-  - this is because JSON keys are in camelCase and Microsoft follows that convention
+test in postman

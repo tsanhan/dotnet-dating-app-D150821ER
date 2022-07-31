d34c00106ca546954a5dfad0584fcae0d46fff06
akunh121 <85361952+akunh121@users.noreply.github.com>
2022-07-31 21:03:35 +0300

    31.07.22

---
+++ DatingApp/API/-- SQLite.sql
@@ -0,0 +1,3 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Bob"), (2, "Avi"),(3, "Nil");
---
+++ DatingApp/API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+        public DbSet<AppUser> Users { get; set; }
+    }
+}

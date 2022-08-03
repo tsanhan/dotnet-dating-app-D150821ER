7c2cc1bcaba927ce106e01bd27ce091e64025f47
Victor Yampolsky <tsanhan@gmail.com>
2022-08-03 21:18:49 +0300

    23. Making HTTP requests in Angular

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title: string = 'The Dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      // response => {
+      //   this.users = response;
+      // },
+      // error => {
+      //   console.log(error);
+      // },
+      // () => {
+      //   console.log('Finished');
+      // }
+      {
+        next: response => {this.users = response;}, // what to do with returned data
+        error: error => {console.log(error);}, // what to do with error
+        complete: () => {console.log('Finished');} // what to do when finished
+      }
+    )
+  }
+
 }
--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-extensions for vscode:
-Angular Language Service
-Angular Snippets by John Papa
-Bracket Pair Colorizer 2
+so we are going to fetch data from our api
+* add module => go to app.module.ts
+* angular uses DI the way .net core uses it.
+* go to app.component.ts
 
+* after running check out the browser to see result
+* no data (it's ok, we didn't interpolate 'users')
+* we DO have errors:
+* we do go to the error part with the printing of the error response
+* but WTF is CORS?
+    - the server tells the browser that it accepts requests only from same origin (url+port)
+    - the browser is the one enforces the policy
+    - https://www.youtube.com/watch?v=4KHiSt0oLJ0
 
-
-
+next up: adding cors to our API

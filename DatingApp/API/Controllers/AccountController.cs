0ad778e671c08656fd487d509db089de19736e0b
Victor Yampolsky <tsanhan@gmail.com>
2022-08-28 19:32:20 +0300

    39. Adding a login endpoint

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -37,6 +37,27 @@ namespace API.Controllers
 
         }
 
+
+        [HttpPost("login")] // POST api/account/login
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+            
+            // get user from db
+            var user = await this._context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+            if(user == null) return Unauthorized("Invalid username or password");
+
+            // check password... so using hmac todo the reverse of what we did to register
+            // calculate the hash using the salt and the given password
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid username or password");
+            }
+
+            return user;
+        }
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-validation:
-    * we can add validation in different levels
-        - db level
-        - entity level (not really relevant in our case, cant validate hash/salt)
-    * we'll use the level of the dto
-    * we can validate before the dto reach the action(endpoint), it's a feature of the ApiController attribute
-    * goto RegisterDto
-    * test the empty username (also with both username & password empty) in postman... we should get an status 400  error
-
-next up: the login method
+adding the login method.

b8127e94f44cec7b1b6ad075b95c751d7765f2d4
Victor Yampolsky <tsanhan@gmail.com>
2022-08-31 19:22:08 +0300

    43. Creating a User DTO and returning the token

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,19 +12,22 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")] // POST api/account/register
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
             using var hmac = new HMACSHA512();
 
-            var user =  new AppUser {
+            var user = new AppUser
+            {
                 UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
@@ -33,17 +37,19 @@ namespace API.Controllers
 
             await _context.SaveChangesAsync();
 
-            return user;
-
+            return new UserDto {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
-
         [HttpPost("login")] // POST api/account/login
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
-            
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+
             // get user from db
             var user = await this._context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
-            if(user == null) return Unauthorized("Invalid username or password");
+            if (user == null) return Unauthorized("Invalid username or password");
 
             // check password... so using hmac todo the reverse of what we did to register
             // calculate the hash using the salt and the given password
@@ -53,15 +59,19 @@ namespace API.Controllers
 
             for (int i = 0; i < computedHash.Length; i++)
             {
-                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid username or password");
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid username or password");
             }
 
-            return user;
+            return new UserDto {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
+        
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
-       
+
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-implementing the login in TokenService.cs
-go to TokenService.cs
+go to AccountController.cs
 
-next up: returning the token to our users when they login
+if we try it now we'll get an error because we need to token key in the configuration (remember? in the constructor in the token service)
+
+go to appsettings.Development.json
+
+test with postman: login
+if we take the returned string and go to jwt.io and use the debugger to see our token, the expiry
+we can verify the signature by pasting our super secret string, if we get the save token then the secret string is correct (the server got the token without change by the front end).
+
+next up: how do we authenticate and validate the token is good.
 

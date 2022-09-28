2df14d9388b673020e50d97d30b571be44fb106c
vic <tsanhan@gmail.com>
2022-09-28 18:01:44 +0300

    85. Extending the user entity

--- DatingApp/API/Entities/Photo.cs
+++ DatingApp/API/Entities/Photo.cs
@@ -10,7 +10,6 @@ namespace API.Entities
         public int Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
-
         public string PublicId { get; set; }
     }
 }

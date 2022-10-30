21295b9db57f54c63aa197587ae8aca6a98ae090
vic <tsanhan@gmail.com>
2022-10-30 18:35:33 +0200

    128. Updating the users controller

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,11 +21,15 @@ namespace API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+        private readonly IPhotoService _photoService;
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
         {
-            _userRepository = userRepository;
+            _photoService = photoService;
             _mapper = mapper;
+            _userRepository = userRepository;
+
         }
+
 
         [HttpGet] // api/users
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -43,7 +49,7 @@ namespace API.Controllers
         [HttpPut] // api/users PUT
         public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDTO)
         {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var username = User.GetUsername();
             var user = await _userRepository.GetUserByUserNameAsync(username);
 
             _mapper.Map(memberUpdateDTO, user);
@@ -53,6 +59,36 @@ namespace API.Controllers
             if (await _userRepository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Failed to update user");
+        }
+
+        [HttpPost("add-photo")] // api/users/add-photo
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var username = User.GetUsername();
+
+            var user = await _userRepository.GetUserByUserNameAsync(username);
+
+            var result = await _photoService.UploadPhotoAsync(file);
+
+            if (result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+
+            photo.IsMain = user.Photos.Count == 0;
+
+            user.Photos.Add(photo);
+
+            if (await _userRepository.SaveAllAsync())
+            {
+                 return _mapper.Map<PhotoDto>(photo);
+            }
+            return BadRequest("Problem adding photo");
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-Adding a photo service:
-* this service will have the single responsibility to manage the photo uploads/deletion
-* when we create a service we first create an interface for it.
-* create and go to interfaces/IPhotoService.cs
-* create and go to Services/PhotoService.cs
+Updating the users controller:
+go to UsersController.cs
 
-up next: using the service in the users controller.
+up next: test AddPhoto using the debugger

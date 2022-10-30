32536131c346e5201bddf255fe5c227842eb8dd6
vic <tsanhan@gmail.com>
2022-10-30 21:01:51 +0200

    132. Adding a photo uploader

--- DatingApp/client/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp/client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,19 +1,63 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader, FileUploadModule } from 'ng2-file-upload';
+import { take } from 'rxjs/operators';
 import { Member } from 'src/app/models/member';
+import { User } from 'src/app/models/user';
+import { AccountService } from 'src/app/services/account.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
   styleUrls: ['./photo-editor.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, FileUploadModule]
 })
 export class PhotoEditorComponent implements OnInit {
+  uploader:FileUploader;
+  hasBaseDropZoneOver:boolean;
+  baseUrl = environment.apiUrl;
+  user: User;
+
+
+
   @Input() member: Member;
-  constructor() { }
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  }
+
 
   ngOnInit() {
+    this.initializeUploader();
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    }
+  }
+
+  fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
   }
 
 }
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-Adding a photo editor component:
-* now we can add the client side to the party:
-* create a component in members name 'photo-editor'. (make sure it;s imported in the app module - it's being used in 'member' route, not 'members' - where the shared module is pointed from)
-* go to photo-editor.component.ts
+adding a photo uploader in the client:
 
-up next: adding a photo uploader in the client
+* to add the photo upload component we'll take a look at a ng2-file-upload package 
+* npm package is pretty popular.
+* we can look at the site: https://valor-software.com/ng2-file-upload
+* it gives us a way to upload files to the server, via drop zone, or via a file input.
+* we can go over the ts file and see how it works:
+    * variables for the dropzone
+    * variable for FileUploader object that accept properties and basically that's it.
+
+
+* ok so run `npm i ng2-file-upload`;
+* go to shared.module.ts to add the module
+* go to photo-editor.component.html
+
+* ok so we done with the ts part, up next see what we need to do in the template part.
